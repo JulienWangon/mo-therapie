77322d83f7f52c14ade36f248740baf6a850308c
Julien Wangon <julien.wangon@gmail.com>
2024-07-19 13:28:56 +0200

    Feat: Création du composant Button

---
+++ mo-front/src/components/common/Button/Button.js
@@ -0,0 +1,16 @@
+import './button.css'
+
+const Button = ({ className, type = "button", colorStyle, onClick, disabled = false, children }) => {
+  return (
+    <button
+      type={type}
+      className={`${className} ${colorStyle}`}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
