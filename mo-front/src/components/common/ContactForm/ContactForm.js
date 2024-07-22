1cdaabc51385b9a0619ebe2d4a18565aa716e12a
Julien Wangon <julien.wangon@gmail.com>
2024-07-22 16:11:43 +0200

    Fix: ajout des label pour les inputs du formulaire

--- mo-front/src/components/common/ContactForm/ContactForm.js
+++ mo-front/src/components/common/ContactForm/ContactForm.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Button from '../../common/Button/Button';
 import './contactForm.css';
 
@@ -6,22 +7,27 @@ const ContactForm = () => {
     <form className="form">
       <h3 className="contactTitle">Contact</h3>
       <div className="input-group">
-        <input type="text" id="lastName" placeholder="Nom" required />
+        <input type="text" id="lastName" required />
+        <label htmlFor="lastName">Nom</label>
       </div>
       <div className="input-group">
-        <input type="text" id="firstName" placeholder="Prénom" required />
+        <input type="text" id="firstName" required />
+        <label htmlFor="firstName">Prénom</label>
       </div>
       <div className="input-group">
-        <input type="email" id="email" placeholder="Email" required />
+        <input type="email" id="email" required />
+        <label htmlFor="email">Email</label>
       </div>
       <div className="input-group">
-        <input type="text" id="sujet" placeholder="Sujet" required />
+        <input type="text" id="sujet" required />
+        <label htmlFor="sujet">Sujet</label>
       </div>
       <div className="input-group messageInput">
-        <textarea cols="30" rows="1" id="message" placeholder="Message" required></textarea>
+        <textarea cols="30" rows="1" id="message" required></textarea>
+        <label htmlFor="message">Message</label>
       </div>
       <div className="submitArea">
-        <Button text="Envoyer" type="submit" colorStyle="purpleBtn" className="submitContact" />
+        <Button type="submit" colorStyle="purpleBtn" className="submitContact">Envoyer</Button>
       </div>
     </form>
   );
