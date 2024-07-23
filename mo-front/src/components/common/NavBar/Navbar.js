ae967d065969dc947cb5ae6c196dc44cbf8c0d4c
Julien Wangon <julien.wangon@gmail.com>
2024-07-23 13:35:27 +0200

    Feat: Création du composant Navbar

---
+++ mo-front/src/components/common/NavBar/Navbar.js
@@ -0,0 +1,93 @@
+import './navbar.css';
+import { useNavigate } from 'react-router-dom';
+
+import SocialMedia from '../SocialMedia/SocialMedia';
+import Button from '../Button/Button';
+import ContactInfo from '../ContactInfo/ContactInfo';
+
+const Navbar = () => {
+  const navigate = useNavigate();
+
+  const links = [
+    { text: "ACCUEIL", url: "/accueil"},
+    { text: "QUI SUIS-JE", url: "/mon-parcours-professionnel" },
+    { text: "THERAPIE BRÈVE SYSTEMIQUE", url: "/therapie-breve-systemique" },
+    { text:"CONSTELLATIONS FAMILIALES", url:"/constellations-familiales"},
+  ];
+
+  const handleNavigate = (url) => {
+    navigate(url);
+  };
+
+  return (
+    <nav className="navbar bg-body-tertiary fixed-top">
+      <div className="container-fluid">
+        <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="offcanvas offcanvas-start" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+          <div className="offcanvas-header">     
+            <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div className="offcanvas-body">
+            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+              {links.map((link, index) => (
+              <li className="navItem" key={index}>
+                <a 
+                    href={link.url} 
+                    onClick={(e) => {
+                      e.preventDefault();
+                      handleNavigate(link.url);
+                      }} 
+                    className="navLink"
+                >
+                  {link.text}
+                </a>
+              </li>
+              ))}
+            </ul>
+                  
+            <SocialMedia className="cardNavbar"/> 
+            <div className="rdvBtncontainer">
+              <Button className="rdvNav"><a className="rdvLink" href="https://www.crenolibre.fr/prendre-rdv/89991_psychopraticienne">Prendre rendez-vous</a></Button>
+            </div>
+            <div className="canvas-footer">
+              <div className="infoContainer">
+                <div className="hoursContainer">
+                  <div className="hoursInfo">  
+                    <i className="fa fa-2x fa-clock"></i>
+                    <span className="hoursText">Horaires</span> 
+                  </div>
+                  <div className="hoursList">
+                    <ul className="listHour">
+                      <li className="hourItem">Lundi, Mardi, Jeudi : 09h-13h; 14h-18h </li>
+                      <li className="hourItem">Mercredi : 08h-12h</li>
+                      <li className="hourItem">Vendredi : 14h-20h</li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="permTel">
+                  <p className="permTelText">Rendez-vous téléphonique gratuit et sans engagement: <a className="permLien" href="https://www.crenolibre.fr/prendre-rdv/89991_psychopraticienne">Ici</a></p>
+                </div>
+                <div className="contactNavbar">
+                  <i className="fa fa-2x fa-map-marker-alt"></i>
+                  {contactInfo.map((info) => (
+                  <ContactInfo
+                    key={info.id}
+                    numero={info.number}
+                    rue={info.street}
+                    codePostal={info.zipCode}
+                    ville={info.city}  
+                  />
+                  ))}
+                </div>
+              </div>
+            </div>                     
+          </div>
+        </div>
+      </div>
+    </nav>
+  ); 
+};
+
+export default Navbar;
